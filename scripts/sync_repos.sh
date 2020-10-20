2c1b141f58043151dea7533cbea0171811e2cdd6
Jerry George <jerrygb@gmail.com>
2020-10-20 14:07:30 -0400

    Fix job name list

--- scripts/sync_repos.sh
+++ scripts/sync_repos.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 REPO_NAME=${1:-scale-ci-pipeline}
 git clone https://github.com/innovation-sre/${REPO_NAME}
+cd ${REPO_NAME}
 git remote add origin-rbc http://rbcgithub.fg.rbc.com/SFT0/${REPO_NAME}
 git push origin-rbc master
+cd ..
